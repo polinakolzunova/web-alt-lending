6a340e7847d850ad39636e32bf6d179b51879d26
Полина Колзунова <polinakolzunova@gmail.com>
2020-01-18 04:39:00 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(() => {
 
     $(".how-work__item-text .icon-cross").click(function () {
         let forchbx = $(this).attr("data-for");
-        console.log(1,forchbx);
+        console.log(1, forchbx);
         $(`#${forchbx}`).prop("checked", false);
     });
 
@@ -17,13 +17,52 @@ $(() => {
 
     function slider(dir = "r") {
         let left = Math.abs($(".clients__list").position().left);
-        let widthel = $(".clients__item").width();
-        let widthlist = $(".clients__list").width();
+        let widthel = $(".clients__item").innerWidth();
 
-        if (left + widthel < widthlist && dir == 'r')
+        let kOnScreen = Math.ceil($(".clients__slider").width() / widthel);
+        let kAll = $(".clients__list").width() / widthel;
+        let kLeft = left / widthel;
+
+        //console.log(kAll, kLeft, kOnScreen);
+
+        if (kAll - kLeft > kOnScreen && dir == 'r')
             $(".clients__list").css({left: "-" + (left + widthel) + "px"});
+
         else if (left >= widthel && dir == 'l')
             $(".clients__list").css({left: "-" + (left - widthel) + "px"});
     }
+
+    $("#featuresPayment").click(function () {
+        $(".features__buttons button").removeClass("active");
+        $(this).addClass("active");
+
+        $(".features-images__item").fadeOut();
+        $(".features-images__item_payment").fadeIn();
+
+        $(".features-content__item").css("display", "none");
+        $(".features-content__item_payment").fadeIn();
+    });
+
+    $("#features1C").click(function () {
+        $(".features__buttons button").removeClass("active");
+        $(this).addClass("active");
+
+        $(".features-images__item").fadeOut();
+        $(".features-images__item_1c").fadeIn();
+
+        $(".features-content__item").css("display", "none");
+        $(".features-content__item_1c").fadeIn();
+    });
+
+    $("#featuresSecurity").click(function () {
+        $(".features__buttons button").removeClass("active");
+        $(this).addClass("active");
+
+        $(".features-images__item").fadeOut();
+        $(".features-images__item_security").fadeIn();
+
+        $(".features-content__item").css("display", "none");
+        $(".features-content__item_security").fadeIn();
+    });
 
 });
